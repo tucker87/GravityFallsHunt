7854d5cb8b5755939035215ad37cb9a4d05c3330
Jason Tucker <jtgeek@gmail.com>
2025-11-21 19:06:15 -0500

    Rewrite using regex

--- t9.js
+++ t9.js
@@ -1,4 +1,5 @@
 const table9 = {
+  ' ': [' '],
   2: ['a', 'b', 'c'],
   3: ['d', 'e', 'f'],
   4: ['g', 'h', 'i'],
@@ -10,30 +11,11 @@ const table9 = {
 }
 
 const t9 = s => {
-  let prev = null
-  let count = 0
-  let answer = ''
-
-  for (const c of s) {
-    if (c === prev) {
-      count++
-    } else {
-      if (prev !== null) {
-        answer += table9[prev][count]
-      }
-      prev = c
-      count = 0
-
-      if (!c.match(/[0-9]/)) {
-        answer += c
-        prev = null
-        continue
-      }
-    }
-  }
-
-  answer += table9[prev][count]
-
+  const matches = s.match(/([0-9]|\s)\1*/g)
+  const answer = matches.reduce(
+    (ans, m) => ans + table9[m[0]][m.length - 1],
+    '',
+  )
   console.log(answer)
 }
 
