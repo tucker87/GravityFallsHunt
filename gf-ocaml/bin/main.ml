38f7aa783696de15b037a644e30f65e66d487ce7
Jason Tucker <jtgeek@gmail.com>
2025-11-25 22:37:49 -0500

    Remove some parens

--- gf-ocaml/bin/main.ml
+++ gf-ocaml/bin/main.ml
@@ -10,7 +10,7 @@ let () =
    "wjz ok ep xacejo" |> ceaser 4 |> log;
 
    log "\nS02 E02";
-   "qi erh xli fscw ex xas eq" |> ceaser(-4) |> log;
+   "qi erh xli fscw ex xas eq" |> ceaser (-4) |> log;
 
    log "\nS02 E03";
    "rg nzb yv wruurxfog gl xlmgrmfv zolmv" |> atbash |> log;
@@ -20,24 +20,24 @@ let () =
    "6633998 933:3355 84433 2224425555553366433 4447777 3666882255533:3" |> t9 |> log;
 
    log "\nDiscord: Mortius Role";
-   "dm hbvc ai qkmec awjy Kzsxrw Xzwhtxuk!" |> (vigenere "cipherhunters") |> log;
+   "dm hbvc ai qkmec awjy Kzsxrw Xzwhtxuk!" |> vigenere "cipherhunters" |> log;
 
    log "\nDiscord: Mortius Role 2";
-   "Qofm arkkvv dlf'k wulbvt gvbtuxzgu bram" |> (vigenere "huntingciphers") |> log;
+   "Qofm arkkvv dlf'k wulbvt gvbtuxzgu bram" |> vigenere "huntingciphers" |> log;
 
    log "\nDiscord: Casper Role";
-   "Tlujf facv qfht xr xvrpsuy uje Olhluteuyft!" |> (vigenere "AEDFBC") |> log;
+   "Tlujf facv qfht xr xvrpsuy uje Olhluteuyft!" |> vigenere "AEDFBC" |> log;
 
    log "\nDiscord:  Will discord puzzles ever interact with episode puzzles";
    "mwB azdbyvr lznwbvs kjlve zbdzdozpsqi naclmom ,zmmwfvo yjp znk mopyvfz yazjomb fgvu MQIME zzvanma zjk oqiwscl pp q omcbcvn nsreao"
-   |> (vigenere "reverse")
+   |> vigenere "reverse"
    |> atbash
    |> Utils.reverse_words
    |> log;
 
    log "\nWould you rather have unlimited bacon, but no games? Or games, unlimited games, but no games?";
    "homozv urquom jzjfre vammnj uwqvay jzjuke hnmjzj uqehom jnjgiq vfmjnv ufjvom jnjtre vaxvnv uiqhom anjufe vayjsj ufehny uzjure vapvnv urqmom jnogfe uamjzv ufqgom vnjxfe homjev ufjhom jnvtfe hoxjzj ufqhap jzjuf qvfmjz jgkqha yjnvtr evamun vufhva yanjgf eaamjn jgreua mvzuuf evopjn juwqho monjtfe vaxjnj uiqhfmj"
-   |> (vigenere "unlimited")
+   |> vigenere "unlimited"
    |> Utils.replace_all {|[^b|n|\s]|} ""
    |> bacon "b" "n"
    |> log;
