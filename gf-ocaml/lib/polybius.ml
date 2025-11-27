628b33bf125ead75c13a9512b0455cb2716ce76e
Jason Tucker <jtgeek@gmail.com>
2025-11-27 04:27:25 -0500

    Got rid of the tables :D

--- gf-ocaml/lib/polybius.ml
+++ gf-ocaml/lib/polybius.ml
@@ -1,28 +1,24 @@
+open Utils
+
 type index =
    | Number of int
    | Pair of int * int
    | Space
 
-let standard_table = [|
-   [|"V"; "A"; "F"; "L"; "Q"|];
-   [|"W"; "B"; "G"; "M"; "R"|];
-   [|"X"; "C"; "H"; "N"; "S"|];
-   [|"Y"; "D"; "I"; "O"; "T"|];
-   [|"Z"; "E"; "K"; "P"; "U"|]|]
+let handle_ij x = if x >= 9 then x + 1 else x
 
-let rotated_table = [|
-   [|"E"; "A"; "B"; "C"; "D"|];
-   [|"K"; "F"; "G"; "H"; "I"|];
-   [|"P"; "L"; "M"; "N"; "O"|];
-   [|"U"; "Q"; "R"; "S"; "T"|];
-   [|"Z"; "V"; "W"; "X"; "Y"|]|]
+let standard_formula x y =
+   (y - 6) * 5 + x - 1 |> handle_ij |> from_a0 65 |> string_of_char
+
+let rotated_formula x y =
+   (x - 1) * 5 + y - 6 |> handle_ij |> from_a0 65 |> string_of_char
 
 let lookup table x y = table.(x-1).(y-5)
 
-let search lookup_t = function
-      | Number x -> lookup_t x 5
-      | Pair (x, y) when x < y -> lookup_t x y
-      | Pair (x, y) -> lookup_t y x
+let search f = function
+      | Number x -> f x 10
+      | Pair (x, y) when x < y -> f x y
+      | Pair (x, y) -> f y x
       | Space -> " "
 
 let digit = Char.Ascii.digit_to_int
@@ -31,15 +27,15 @@ let rec string_to_pairs = function
       | [] -> []
       | ' ' :: rest -> Space :: string_to_pairs rest
       | a :: b :: rest when b = ' ' -> Number (digit a) :: Space :: string_to_pairs rest
-      | a :: b :: rest when b = 'x' -> Number (digit a) :: string_to_pairs rest
+      | a :: b :: rest when b = '0' -> Number (digit a) :: string_to_pairs rest
       | a :: b :: rest -> Pair (digit a, digit b) :: string_to_pairs rest
       | a :: [] -> Number (digit a) :: []
 
-let _run table input =
-   input |> Utils.explode |> string_to_pairs |> List.map @@ search (lookup table) |> String.concat ""
+let _run formula input =
+   input |> Utils.explode |> string_to_pairs |> List.map @@ search formula |> String.concat ""
 
 let run input =
-   _run standard_table input
+   _run standard_formula input
 
 let run_rotated input =
-   _run rotated_table input
+   _run rotated_formula input
