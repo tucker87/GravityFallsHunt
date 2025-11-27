389cfcaf2b2d3dcec9cd800d5a8ea5c4772d9487
Jason Tucker <jtgeek@gmail.com>
2025-11-27 18:39:30 -0500

    Found a simpler explode function

--- gf-ocaml/lib/utils.ml
+++ gf-ocaml/lib/utils.ml
@@ -36,13 +36,8 @@ let increment_letter i c =
 
 let string_of_char c = String.make 1 c 
 
-let explode str =
-  let rec explode_inner cur_index chars = 
-    if cur_index < String.length str then
-      let new_char = str.[cur_index] in
-      explode_inner (cur_index + 1) (chars @ [new_char])
-    else chars in
-  explode_inner 0 []
+let explode s =
+   List.init (String.length s) (String.get s)
 
 let rec implode chars =
   match chars with
