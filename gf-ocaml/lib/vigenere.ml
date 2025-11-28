02759871efe465b24ab3d6ca88da5c8056b94824
Jason Tucker <jtgeek@gmail.com>
2025-11-28 11:53:45 -0500

    Learned about dispensers. Reworked Vigenere using them

--- gf-ocaml/lib/vigenere.ml
+++ gf-ocaml/lib/vigenere.ml
@@ -1,17 +1,13 @@
 open Utils
 
-let plusplus_wrap len i =
-   let idx = !i in
-   i := (!i+1) mod len;
-   idx
+let zip_when f d xs ys =
+   let y_dispenser = Seq.to_dispenser ys in
+   xs |> Seq.map (fun x -> if f x then (x, y_dispenser () |> Option.get) else (x, d))
 
-let next_offset k = 
-   let i = ref 0 in
-   let pp = plusplus_wrap @@ String.length k in
-   fun () -> String.get k (pp i) |> char_to_letter |> letter_to_int |> Int.neg
-
-let _run get_offset c = (get_offset () |> increment_letter)  c
+let char_to_negative c =
+   c |> char_to_letter |> letter_to_int |> Int.neg
 
 let run k s =
-   (next_offset k |> _run |> skip_non_alpha |> String.map) s
-
+   zip_when is_alpha 0 (String.to_seq s) (String.to_seq k |> Seq.map char_to_negative |> Seq.cycle) 
+   |> Seq.map (fun (a, b) -> increment_letter b a)
+   |> String.of_seq
