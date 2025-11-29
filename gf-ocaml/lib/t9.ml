c7da47255716196a01115f2dbc0eb1c1920cdc55
Jason Tucker <jtgeek@gmail.com>
2025-11-28 23:01:23 -0500

    Rewrote t9 again

--- gf-ocaml/lib/t9.ml
+++ gf-ocaml/lib/t9.ml
@@ -1,13 +1,18 @@
-let lookup_match m =
-  let c = String.get (Pcre2.get_substring m 1) 0 in
-  if Char.Ascii.is_digit c then
-    let offset = (String.length @@ Pcre2.get_substring m 0) - 1 in
-    let d = Char.Ascii.digit_to_int c in
-    (((d - 2) * 3) + offset + if d > 7 then 1 else 0)
-    |> A0.lower_of_int |> A0.to_char
-  else c
+let get_info ms =
+  let s = Pcre2.get_substring ms 0 in
+  String.get s 0, String.length s - 1
+
+
+let lookup_match (c, offset) =
+  match A0.of_char c with
+  | Digit d ->
+    (((d - 2) * 3) + offset + if d > 7 then 1 else 0) |> A0.lower_of_int |> A0.to_char
+  | _ -> c
+
 
 let run s =
-  Pcre2.exec_all
-    ~pat:{|([0-9]|\s)((?<=[0-6|8])\1{0,2}|(?<=[7|9])\1{0,3}|(?<=\s))|} s
-  |> Array.map lookup_match |> Array.to_seq |> String.of_seq
+  Pcre2.exec_all ~pat:{|([0-9]|\s)((?<=[0-6|8])\1{0,2}|(?<=[7|9])\1{0,3}|(?<=\s))|} s
+  |> Array.map get_info
+  |> Array.map lookup_match
+  |> Array.to_seq
+  |> String.of_seq
